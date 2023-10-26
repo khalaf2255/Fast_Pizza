7fe2720e271b6017542951e7486ff9d2161cfecc
Ahmed Khalaf <ahmed.khalaf.saleh@gmail.com>
2023-10-26 12:15:52 +0200

    jh

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -2,8 +2,8 @@ import Button from '../../ui/Button';
 import { formatCurrency } from '../../utils/helpers';
 
 function CartItem({ item }) {
-  const { pizzaId, name, quantity, totalPrice } = item;
-
+  const { pizzaId, name, quantity, totalPrice } = item; 
+ 
   return (
     <li className="py-3 sm:flex sm:items-center sm:justify-between">
       <p className="mb-1 sm:mb-0">
